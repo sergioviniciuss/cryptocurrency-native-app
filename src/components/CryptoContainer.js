a069f9d4d32e0f74a071175dbbc8a0edda59ac1c
Sérgio Lucena <sergioviniciuss@gmail.com>
2018-09-16 23:43:45 +0200

    Creating cryptocontainer

---
+++ src/components/CryptoContainer.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import {
+    View,
+    Text
+}
+from 'react-native';
+
+class CryptoContainer extends Component {
+    render() {
+        <View>
+            <Text>Container</Text>
+        </View>
+    }
+}
