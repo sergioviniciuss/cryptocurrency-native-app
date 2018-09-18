7c081d713c29b1c7e0e01ef6f9c75fc00bc87498
Sérgio Lucena <sergioviniciuss@gmail.com>
2018-09-18 22:39:12 +0200

    Implementing the reducer for fetching coin data from api

--- src/Reducers/CryptoReducer.js
+++ src/Reducers/CryptoReducer.js
@@ -1,3 +1,9 @@
+import {
+    FETCHING_COIN_DATA,
+    FETCHING_COIN_DATA_SUCCESS,
+    FETCHING_COIN_DATA_FAIL,
+} from './../utils/actionTypes';
+
 const initialState = {
     isFetching: null,
     data: [],
@@ -6,5 +12,35 @@ const initialState = {
 };
 
 export default function(state = initialState, action) {
+    switch (action.type) {
+        case FETCHING_COIN_DATA:
+            return Object.assign({}, state, {
+                isFetching: true,
+                data: null,
+                hasError: false,
+                errorMessage: null,
+            })
+            break;
+        case FETCHING_COIN_DATA_SUCCESS:
+            return Object.assign({}, state, {
+                isFetching: false,
+                data: action.payload,
+                hasError: false,
+                errorMessage: null,
+            })
+            break;
+        case FETCHING_COIN_DATA:
+            return Object.assign({}, state, {
+                isFetching: false,
+                data: action.payload,
+                hasError: true,
+                errorMessage: action.err,
+            })
+            break;
+    
+        default:
+            return state;
+            break;
+    }
     return state;
 }
